64e250cdffb131010ebbde15633430959c3af6b5
algosig <sigdelsanjog@gmail.com>
2017-04-29 11:56:03 +0545

    Removed non breakable space (nbsp) and added CSS styling Components

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -12,12 +12,14 @@ import { Component, Input } from '@angular/core'
             <div>Price: \${{event.price}}</div>
             <div>
                 <span>Location: {{event.location.address}}</span>
-                <span>&nbsp;</span>
-                <span>{{event.location.city}}, {{event.location.country}}</span>
+                <span class="pad-left"> {{event.location.city}}, {{event.location.country}}</span>
             </div>
         </div>
     </div>
-    `
+    `,
+    styles:[`
+    .pad-left {margin-left:40px;}
+    `]
 })
 
 export class EventThumbnailComponent{
