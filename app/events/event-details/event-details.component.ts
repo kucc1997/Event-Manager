708c4d85a0e222fcd24ac24fd89e9ce492e7d0c0
algosig <sigdelsanjog@gmail.com>
2017-05-03 13:04:02 +0545

    1) Added Activated Route to display route information with specific event id

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import {Component } from '@angular/core'
 import { EventService } from '../shared/event.service'
+import { ActivatedRoute } from '@angular/router'
 @Component({
     templateUrl:
     '/app/events/event-details/event-details.component.html',
@@ -14,13 +15,14 @@ import { EventService } from '../shared/event.service'
 
 export class EventDetailsComponent{
    event:any
-   constructor(private eventService:EventService) {
+   constructor(private eventService:EventService,
+                private route: ActivatedRoute) {
 
    }
 
     ngOnInit() {
         //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
         //Add 'implements OnInit' to the class.
-        this.event = this.eventService.getEvent(1)
+        this.event = this.eventService.getEvent( +this.route.snapshot.params['id'])
     } 
 }
