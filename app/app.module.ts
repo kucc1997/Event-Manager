642f580291e86c4996f6594a0a2e2fbce0c39c2b
algosig <sigdelsanjog@gmail.com>
2017-05-03 15:07:50 +0545

    1) Adden new 'Create Event Component" 2) Defined Route to events after Cancel key pressed

--- app/app.module.ts
+++ app/app.module.ts
@@ -10,6 +10,7 @@ import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { appRoutes } from './routes'
+import { CreateEventComponent } from './events/create-event.component'
 
 @NgModule({
 	imports: [BrowserModule,
@@ -20,7 +21,8 @@ import { appRoutes } from './routes'
 			EventslistComponent,
 			EventThumbnailComponent,
 			NavBarComponent,
-			EventDetailsComponent
+			EventDetailsComponent,
+			CreateEventComponent
 		],
 		providers: [EventService, ToastrService],
 		bootstrap : [EventsAppComponent]
