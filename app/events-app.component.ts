2c7f0d5fd84e6da7cf5a4906a969ed899eaeb861
algosig <sigdelsanjog@gmail.com>
2017-05-19 15:37:14 +0545

    #Chardin href=" " rakhera pidit vaye naya usermodule banauda thuikka jindagani. :/ 1) Added new module userModule

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core'
+import { NavBarComponent } from './nav/navbar.components'
 @Component({
-	selector: 'events-app',
+	selector: "events-app",
 	template:`
 		<nav-bar></nav-bar>
 		<router-outlet></router-outlet>
