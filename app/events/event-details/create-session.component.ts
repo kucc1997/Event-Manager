8d41b7a1462fa9577534a8ae7a87466b1115077e
algosig <sigdelsanjog@gmail.com>
2017-06-01 17:35:23 +0545

    Minor Fix, mistakenly assigned formControlName to label, Duration

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -17,7 +17,7 @@ export class CreateSessionComponent implements OnInit {
         this.presenter = new FormControl('', Validators.required)
         this.duration = new FormControl('', Validators.required)
         this.level = new FormControl('', Validators.required)
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)])
+        this.abstract = new FormControl('', Validators.required, [Validators.maxLength(400)] )
     
     this.newSessionForm = new FormGroup({
         name: this.name,
